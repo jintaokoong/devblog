6276e5430e51aebf6bee5a7f5fcfb26c48680fcf
Jintao Koong <jintaokoong99@gmail.com>
2024-01-19 11:27:13 +0800

    chore: fix meta tags, site title and desc

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Henry's Blog";
+export const SITE_DESCRIPTION =
+  "Software Engineer based in Kuala Lumpur, Malaysia. I write about software development, productivity, and life.";
